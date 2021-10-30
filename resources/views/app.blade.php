ea136836f735027e34b043f472efd3a277162761
John Mwakalinga <mwakalingajohn@gmail.com>
2021-10-30 22:28:59 +0300

    - test mix resource

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('/awesome/css/app.css') }}">
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ mix('/awesome/js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
